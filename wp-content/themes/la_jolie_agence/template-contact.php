294feb9b4270c32b03b3c2f1e27a4ba4168baf1f
QVan27 <qvannarath@gmail.com>
2021-08-20 00:17:01 +0200

    finition page contact

--- wp-content/themes/la_jolie_agence/template-contact.php
+++ wp-content/themes/la_jolie_agence/template-contact.php
@@ -58,7 +58,11 @@ get_header(); ?>
         <div class="box-reseaux">
             <a href="https://www.facebook.com/lajolieagence" target="_blank"><i class="about fab fa-facebook-square fa-3x"></i></a>
             <a href="https://www.instagram.com/lajolieagence/" target="_blank"></i><i class="about fab fa-instagram fa-3x"></i></a>
-            <!-- <a href=""><i class="about fab fa-youtube fa-3x"></i></a> -->
+        </div>
+
+        <div class="box-text">
+            <p>Racontez-nous votre histoire, nous nous ferions un plaisir de vous accompagner dans votre projet d’organisation.<br>La Jolie Agence vous accompagne dans l’organisation de votre mariage sur Bordeaux, Pays Basque, Dordogne, Les Landes, Charente, Charente- Maritime.
+            </p>
         </div>
 
         <div class="box-content-text content-text-contact">
@@ -122,13 +126,18 @@ get_header(); ?>
                                                 echo $errors['message'];
                                             } ?></span>
                     </div>
-                    <input type="submit" name="submitted" value="Envoyer">
+                    <input type="submit" id="submit" name="submitted" value="Envoyer">
                 </form>
             </div>
         </div>
+
+        <div class="box-text">
+            <p>N’hésitez pas à nous contacter directement par mail ou par téléphone.<br>Nous pouvons nous rencontrer à votre domicile, autour d’un verre, thé, café…<br>Horaires : Lundi au Vendredi de 9h à 19 h</p>
+        </div>
+
     </div>
     <div class="map">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22448.238338589617!2d-0.6181986141107647!3d45.30782379478757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48004ab0321b897d%3A0x40665174816d520!2s33820%20Saint-Palais!5e0!3m2!1sfr!2sfr!4v1629314907134!5m2!1sfr!2sfr" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22448.238338589617!2d-0.6181986141107647!3d45.30782379478757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48004ab0321b897d%3A0x40665174816d520!2s33820%20Saint-Palais!5e0!3m2!1sfr!2sfr!4v1629314907134!5m2!1sfr!2sfr" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 </div>
 
