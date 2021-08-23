14dce53cf8966d9fec04392120af6534572fd974
QVan27 <qvannarath@gmail.com>
2021-08-23 23:03:03 +0200

    fix image cover dons

--- wp-content/themes/la_jolie_agence/template-dons.php
+++ wp-content/themes/la_jolie_agence/template-dons.php
@@ -8,7 +8,9 @@ get_header(); ?>
 <section class="don">
     <div class="content-about">
         <div id="text-about">
-            <div id="save"><h1>La Jolie Agence</h1></div>
+            <div id="save">
+                <h1>La Jolie Agence</h1>
+            </div>
         </div>
         <div class="married">
             <h2>Dons Associatifs</h2>
@@ -28,18 +30,18 @@ get_header(); ?>
         <div class="box-content-text">
             <div class="minibox-text marginBottom">
                 <p>Derrière La Jolie Agence se cache Adeline. Avant d’être wedding planner, Adeline a organisé plusieurs types d'événements et notamment un voyage de 3 mois à but humanitaire en 2014.<br><br>Adeline a visité de nombreux pays d’Europe et du Monde sous différentes manières : humanitaire, en backpack… Toujours à la découverte d’une nouvelle culture, d’un nouvel univers, de soi aussi ... et parce qu’explorer le monde est tellement enrichissant de n’importe quelle façon. <br>
-                Le Burkina Faso a été l’un de ses tout premiers voyages. Après une formation dans l’animation socio-culturelle, Adeline a pu monter un projet autour de l’animation et de la pédagogie : Apprendre & Comprendre par le jeu !<br><br>
-                En partenariat avec l’association Alliance Burkina Bray qui existe depuis 1997 à Sabcé, et œuvre sur différentes thématiques comme La santé, La scolarité, les parrainages scolaires et la formation pour adultes, L'eau, Le reboisement, Le développement économique. 
-Celle-ci a fait le constat de la difficulté des élèves à apprendre et comprendre le français. Le Burkina Faso est une ancienne colonie française, les enfants burkinabés apprennent le français à l’école mais de retour chez eux, ils parlent le mòoré (dialecte burkinabé). 
+                    Le Burkina Faso a été l’un de ses tout premiers voyages. Après une formation dans l’animation socio-culturelle, Adeline a pu monter un projet autour de l’animation et de la pédagogie : Apprendre & Comprendre par le jeu !<br><br>
+                    En partenariat avec l’association Alliance Burkina Bray qui existe depuis 1997 à Sabcé, et œuvre sur différentes thématiques comme La santé, La scolarité, les parrainages scolaires et la formation pour adultes, L'eau, Le reboisement, Le développement économique.
+                    Celle-ci a fait le constat de la difficulté des élèves à apprendre et comprendre le français. Le Burkina Faso est une ancienne colonie française, les enfants burkinabés apprennent le français à l’école mais de retour chez eux, ils parlent le mòoré (dialecte burkinabé).
                 </p>
                 <div class="minibox-img">
                     <style>
                         .minibox-img {
                             background: url(<?= get_template_directory_uri() . '/asset/img/1-dons.jpg' ?>) no-repeat center;
-                            background-size: contain;
+                            background-size: cover;
                             min-height: 100%;
                             max-height: 100%;
-                            
+
                         }
                     </style>
                 </div>
@@ -47,15 +49,15 @@ Celle-ci a fait le constat de la difficulté des élèves à apprendre et compre
 
             <div class="minibox-text reverse marginBottom">
                 <p>A travers le jeu, une organisation dans 4 écoles différentes, avec 60 enfants par classe s’est mise en place pour apprendre des jeux .Une Olympiade a été organisée au milieu de la brousse avec plus de 300 enfants et un large public venus applaudir sa jeunesse.<br><br>
-                L’association parraine des enfants qui ont une famille démunie, ou qui sont orphelins, elle les accompagne et les aide à acheter du matériel scolaire, des vêtements et parfois de la nourriture. L’éducation est primordiale au Burkina, et les études ont un coût.<br><br>
-                Le parrainage permet de poursuivre leur scolarité.  De nombreux enfants parrainés ont eu l’opportunité d’être étudiants dans l’enseignement supérieur dont celles de soutenir une thèse en médecine  <br><br>
-                
+                    L’association parraine des enfants qui ont une famille démunie, ou qui sont orphelins, elle les accompagne et les aide à acheter du matériel scolaire, des vêtements et parfois de la nourriture. L’éducation est primordiale au Burkina, et les études ont un coût.<br><br>
+                    Le parrainage permet de poursuivre leur scolarité. De nombreux enfants parrainés ont eu l’opportunité d’être étudiants dans l’enseignement supérieur dont celles de soutenir une thèse en médecine <br><br>
+
                 </p>
                 <div class="minibox-img minibox-img_2">
-                <style>
+                    <style>
                         .minibox-img_2 {
                             background: url(<?= get_template_directory_uri() . '/asset/img/2-dons.jpg' ?>) no-repeat center;
-                            background-size: contain;
+                            background-size: cover;
                         }
                     </style>
                 </div>
@@ -63,18 +65,18 @@ Celle-ci a fait le constat de la difficulté des élèves à apprendre et compre
 
             <div class="minibox-text reverse">
                 <div class="box-img-dons">
-                    
+
                 </div>
             </div>
 
             <div class="minibox-text center">
                 <div class="speakDons">
-                  <p>Notre objectif est d’aider l’association à se développer et offrir un soutien financier aux enfants burkinabés dans le besoin. L’objectif est de leur permettre de réaliser leur rêve d’études.Nous nous engageons à donner au moins 10 % de notre chiffre d’affaires annuel en continu pour le parrainage scolaire. N’oubliez pas, une nouvelle rencontre c’est un nouveau voyage qui commence ! Vous pouvez en savoir plus sur http://burkinabray.canalblog.com/</p>
+                    <p>Notre objectif est d’aider l’association à se développer et offrir un soutien financier aux enfants burkinabés dans le besoin. L’objectif est de leur permettre de réaliser leur rêve d’études.Nous nous engageons à donner au moins 10 % de notre chiffre d’affaires annuel en continu pour le parrainage scolaire. N’oubliez pas, une nouvelle rencontre c’est un nouveau voyage qui commence ! Vous pouvez en savoir plus sur http://burkinabray.canalblog.com/</p>
                 </div>
             </div>
             <div class="box-img-dons2">
-                    
-                </div>
+
+            </div>
 
         </div>
     </div>
@@ -82,6 +84,3 @@ Celle-ci a fait le constat de la difficulté des élèves à apprendre et compre
 
 <?php
 get_footer();
-
-
-
--- wp-content/themes/la_jolie_agence/template-service1.php
+++ wp-content/themes/la_jolie_agence/template-service1.php
@@ -3,12 +3,19 @@
 Template Name: Service1
 */
 get_header(); ?>
-<section id="contact" data-scroll-section class="parallax-window-about" data-parallax="scroll" data-image-src="<?php echo get_template_directory_uri() . '/asset/img/table.jpg' ?>">
-    <div class="content-about" data-scroll data-scroll-delay="0.04" data-scroll-speed="3">
-        <h1>WEDDING DAY</h1>
-    </div>
+<section class="presta">
+	<div class="content-about">
+		<div id="text-about">
+			<div id="save">
+				<h1>La Jolie Agence</h1>
+			</div>
+		</div>
+		<div class="married">
+			<h2>wedding planner</h2>
+		</div>
+	</div>
 </section>
-<div class="bigbox-about bigbox-contact" data-scroll-section>
+<!-- <div class="bigbox-about bigbox-contact" data-scroll-section>
 	<div class="box-back">
     <a href="<?php echo esc_url(home_url('prestation')); ?>"><i class=" about fas fa-arrow-circle-left fa-3x "></i></a>
     </div>
@@ -80,9 +87,67 @@ get_header(); ?>
 		</div>
     
 	</div>
-</section>
+</section> -->
 
-</>
+<div class="box-content-text p105">
+	<div class="minibox-text marginBottom">
+		<div class="box-presta-text pr5">
+			<h3>Organisation Complète</h3>
+			<p>
+				Vous n'avez pas le temps, les préparatifs vous stressent ? Faites appel à une professionnelle.
+				Nous nous occupons de vous organiser votre journée de A à Z.
+				La Jolie Agence recherche pour vous les prestataires qui vous correspondent, tout en analysant les devis et le côté administratif.
+				Nous proposons des rdv avec les prestataires au plus près de vos attentes jusqu’à la réservation pour votre évènement.Nous élaborons un devis personnalisé et un budget prévisionnel incluant toutes les prestations demandées, y compris la nôtre.
+				La Jolie Agence vous offre toutes ses astuces et savoir-faire pour l’organisation de votre joli jour.
+				A partir de 3 590 €
+				* Présence du matin de l'événement jusqu’au soir après la première danse. La création de la décoration et la désinstallation ne sont pas inclus dans cette prestation.
+			</p>
+		</div>
+
+		<div class="minibox-img">
+			<style>
+				.minibox-img {
+					background: url(<?= get_template_directory_uri() . '/asset/img/1-dons.jpg' ?>) no-repeat center;
+					background-size: cover;
+					min-height: 100%;
+					max-height: 100%;
+				}
+			</style>
+		</div>
+	</div>
+
+	<div class="minibox-text reverse marginBottom">
+		<p>A travers le jeu, une organisation dans 4 écoles différentes, avec 60 enfants par classe s’est mise en place pour apprendre des jeux .Une Olympiade a été organisée au milieu de la brousse avec plus de 300 enfants et un large public venus applaudir sa jeunesse.<br><br>
+			L’association parraine des enfants qui ont une famille démunie, ou qui sont orphelins, elle les accompagne et les aide à acheter du matériel scolaire, des vêtements et parfois de la nourriture. L’éducation est primordiale au Burkina, et les études ont un coût.<br><br>
+			Le parrainage permet de poursuivre leur scolarité. De nombreux enfants parrainés ont eu l’opportunité d’être étudiants dans l’enseignement supérieur dont celles de soutenir une thèse en médecine <br><br>
+
+		</p>
+		<div class="minibox-img minibox-img_2">
+			<style>
+				.minibox-img_2 {
+					background: url(<?= get_template_directory_uri() . '/asset/img/2-dons.jpg' ?>) no-repeat center;
+					background-size: cover;
+				}
+			</style>
+		</div>
+	</div>
+
+	<div class="minibox-text reverse">
+		<div class="box-img-dons">
+
+		</div>
+	</div>
+
+	<div class="minibox-text center">
+		<div class="speakDons">
+			<p>Notre objectif est d’aider l’association à se développer et offrir un soutien financier aux enfants burkinabés dans le besoin. L’objectif est de leur permettre de réaliser leur rêve d’études.Nous nous engageons à donner au moins 10 % de notre chiffre d’affaires annuel en continu pour le parrainage scolaire. N’oubliez pas, une nouvelle rencontre c’est un nouveau voyage qui commence ! Vous pouvez en savoir plus sur http://burkinabray.canalblog.com/</p>
+		</div>
+	</div>
+	<div class="box-img-dons2">
+
+	</div>
+
+</div>
 
 <?php
 get_footer();
